9150d98ed861292b191bf5e0bfc6a35fbd86b65f
pc <pc@pcs-MacBook-Pro.local>
2023-05-12 13:05:13 +0400

    added route page-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./panets/HomePage";
+import AboutPage from "./panets/AboutPage";
+import ContactPage from "./panets/ContactPage";
+import FaqPage from "./panets/FaqPage";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="about" element={<AboutPage />} />
+      <Route path="contact" element={<ContactPage />} />
+      <Route path="fac" element={<FaqPage />} />
+    </Routes>
+  );
 }
 
 export default App;
